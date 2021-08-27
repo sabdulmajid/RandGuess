2947242b044743e86074a163a2f2338355183dd8
sabdulmajid <ayman.hasib@outlook.com>
2021-08-27 22:35:52 +0300

    Changes to game

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -5,32 +5,66 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
 
-    // Welcome message
-    PrintLine(TEXT("Welcome to the Bull-Cow Game!"));
-    PrintLine(TEXT("Guess the 4 letter word!")); // Magic number hardcoded!
-    PrintLine(TEXT("Please enter to continue..."));
+    SetupGame(); // Setting up the game
 
-    // Setting up the game
-    HiddenWord = TEXT("take");
+    PrintLine(TEXT("The HiddenWord is: %s. \nIt is %i characters long."), *HiddenWord, HiddenWord.Len()); // Debug line
 
-    // Settings lives
-
-    // Prompting player for guess
 }
 
-void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
+void UBullCowCartridge::OnInput(const FString &Input) // When the player hits enter
 {
-    ClearScreen();
     
-    if (Input == HiddenWord)
-    {
-        PrintLine(TEXT("Victory! Well done, you won!"));
 
-    }
-    else
-    {
-        PrintLine(TEXT("You lost! Nice try!"));
-    }
     
+    /*
+    If game over, then ClearScreen() and SetupGame().
+    Else check PlayerGuess 
+    */
+   if (bGameOver)
+   {
+        ClearScreen();
+        SetupGame();
+   }
+   else
+   {
+        if (Input == HiddenWord)
+        {
+            PrintLine(TEXT("Victory! Well done, you won!"));
+            // bGameOver = true;
+        }
+        else
+        {
+            if (Input.Len() != HiddenWord.Len())
+            {
+                PrintLine(TEXT("Try again, the hidden word is %i characters long."), HiddenWord.Len()); // Magic number hardcoded!
+            }
+
+            PrintLine(TEXT("You lost! Nice try!"));
+            // bGameOver = true;
+    }
+   }
+
+   
+
     PrintLine(Input);
+}
+
+void UBullCowCartridge::SetupGame()
+{
+    // Welcome message
+    PrintLine(TEXT("Welcome to the Bull-Cow Game!"));
+
+    HiddenWord = TEXT("take");
+    Lives = 4;
+    bGameOver = false;
+    
+    PrintLine(TEXT("Guess the %i letter word!"), HiddenWord.Len()); 
+    PrintLine(TEXT("Type in your guess. \nPress enter to continue...")); // Prompting player for guess
+   
+}
+
+void UBullCowCartridge::EndGame()
+{
+    bGameOver = true;
+    PrintLine(TEXT("Press enter to play again..."))
 }
